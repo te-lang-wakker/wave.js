5bf7c0092d8967f3bf27a305900be57b4e23a814
te lang wakker <38595535+te-lang-wakker@users.noreply.github.com>
2020-10-14 15:39:23 +0200

    Create wave.js

---
+++ wave.js
@@ -0,0 +1,83 @@
+/**
+ * Draws a wave to a specified canvas.
+ *
+ * @author te-lang-wakker
+ */
+
+// config
+
+const XSTART = 0;
+const YSTART = window.innerHeight / 2;
+const STEP_SIZE = 0.01;
+const MAGNITUDE = 15;
+const PERIOD = 200;
+const SPEED = 0.005;
+const VARIATION = 15;
+const NORMALISATION = 1;
+
+// init
+
+let canvas;
+let context;
+
+var offset = 0;
+var variations = [VARIATION];
+
+window.addEventListener("load", init);
+setInterval(draw, 5);
+
+function init() {
+    canvas = document.getElementById("wave");
+    context = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    seedVariations();
+}
+
+// randomise
+
+function seedVariations() {
+    for (let i = 0; i < VARIATION; i++) {
+        variations[i] = 0;
+        for (let j = 0; j < NORMALISATION; j++) {
+            variations[i] += Math.random();
+        }
+        variations[i] /= NORMALISATION;
+    }
+}
+
+// calculate
+
+function cumulativeSin(x, offset) {
+    let returnSin = 0;
+
+    for (let i = 0; i < VARIATION; i++) {
+        returnSin += Math.sin(x * variations[i] + offset);
+    }
+
+    return returnSin / VARIATION;
+}
+
+// draw
+
+function draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.beginPath();
+    context.lineTo(XSTART, YSTART + canvas.height - YSTART);
+
+    let x = 0, y;
+    for (let i = 0; x < canvas.width; i += STEP_SIZE) {
+        x = XSTART + i * PERIOD;
+        y = YSTART + cumulativeSin(i, offset) * MAGNITUDE;
+        context.lineTo(x, y);
+    }
+
+    context.lineTo(x, y + canvas.height - y);
+    context.closePath();
+    context.fillStyle = "#D63C28";
+    context.fill();
+
+    offset += SPEED;
+}
